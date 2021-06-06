dcbfc823d7319c4b553becd7076bf964f4cab1f0
lucas <laksteelhouse@gmail.com>
2021-06-06 15:43:55 +0200

    Writes GET request spec for list of inquiries

---
+++ spec/requests/api/api_can_respond_with_list_of_inquiries_spec.rb
@@ -0,0 +1,47 @@
+RSpec.describe 'GET /api/inquiries', type: :request do
+  describe 'successfully' do
+    before do
+      get '/api/inquiries'
+    end
+
+    it 'is expected to respond with status 200' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'is expected to respond with a list of 3 inquiries' do
+      expect(response_json['inquiries'].count).to eq 3
+    end
+
+    it 'is expected to include the inquiry\'s company size' do
+      expect(response_json['inquiries'].first['size']).to eq 5 
+    end
+
+    it 'is expected to include the inquiry\'s office type' do
+      expect(response_json['inquiries'].first['office_type']).to eq 'office' 
+    end
+
+    it 'is expected to include the inquiry\'s company name' do
+      expect(response_json['inquiries'].first['company']).to eq 'Company1'
+    end
+
+    it 'is expected to include the inquiry\'s peer request' do
+      expect(response_json['inquiries'].first['peers']).to eq true 
+    end
+
+    it 'is expected to include the inquiry\'s email' do
+      expect(response_json['inquiries'].first['email']).to eq 'mrfake1@fake.com' 
+    end
+
+    it 'is expected to include the inquiry\'s peer request' do
+      expect(response_json['inquiries'].first['flexible']).to eq true 
+    end
+
+    it 'is expected to include the inquiry\'s start date' do
+      expect(response_json['inquiries'].first['start_date']).to eq '2021-06-16' 
+    end
+
+    it 'is expected to include the inquiry\'s phone number' do
+      expect(response_json['inquiries'].first['phone']).to eq 0707123456 
+    end
+  end
+end
