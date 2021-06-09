191cf8584edde18a5b2dcb455745dc35f13fa6e8
oliverochman <oliver.ochman@hotmail.com>
2021-06-09 14:35:55 +0200

    add test for inquiry broker association

--- spec/requests/api/provider_can_update_inquiry_spec.rb
+++ spec/requests/api/provider_can_update_inquiry_spec.rb
@@ -1,15 +1,26 @@
 RSpec.describe 'PUT /api/inquiries/:id', type: :request do
   let(:inquiry) { create(:inquiry, inquiry_status: 'pending') }
+
+  let(:user) { create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:user_headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
+
   describe 'successfully updated' do
     before do
       put "/api/inquiries/#{inquiry.id}",
           params: {
             form_data: { inquiry_status: 'started' }
-          }
+          },
+          headers: user_headers
     end
 
     it 'is expected to return a 200 status' do
       expect(response).to have_http_status 200
+    end
+
+    it 'is expected to be associated to user that updated inquiry status' do
+      inquiry.reload!
+      expect(inquiry.broker).to eq user
     end
 
     it 'is expected to respond with a message' do
