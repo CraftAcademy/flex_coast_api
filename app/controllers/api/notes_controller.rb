8049a4e4235a0519a30875a844e5c8328db73d1f
oliverochman <oliver.ochman@hotmail.com>
2021-06-11 15:14:17 +0200

    [WIP] add logic to notes#create action

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,4 +1,15 @@
 class Api::NotesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    inquiry = Inquiry.find(params[:inquiry_id])
+
+    note = current_user.notes.create(note_params).merge(inquiry: inquiry)
+
+    if note.persisted?
+      render json: { message: 'Note successfully created' }
+    else
+      binding.pry
+    end
   end
 end
