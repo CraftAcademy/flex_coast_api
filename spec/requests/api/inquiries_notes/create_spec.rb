851ce76138ea6c4c976bf497531f089a37010192
oliverochman <oliver.ochman@hotmail.com>
2021-06-11 15:10:07 +0200

    update req spec with association to user

--- spec/requests/api/inquiries_notes/create_spec.rb
+++ spec/requests/api/inquiries_notes/create_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe 'POST /api/inquiries/:id/notes', type: :request do
   let(:inquiry) { create(:inquiry, inquiry_status: 'pending') }
+  let(:writer) { create(:user) }
+  let(:credentials) { writer.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+
 
   describe 'successfully' do
     before do
-      post "/api/inquiries/#{inquiry.id}/notes"
+      post "/api/inquiries/#{inquiry.id}/notes",
+      params: {
+        note: {
+          body: 'Dont mention clients hairy nose'
+        }
+      },
+      headers: headers
     end
 
     it 'is expected to return a 200 status' do
@@ -12,6 +22,10 @@ RSpec.describe 'POST /api/inquiries/:id/notes', type: :request do
 
     it 'is expected to respond with a success message' do
       expect(response_json['message']).to eq 'Note successfully created'
+    end
+
+    it 'is expected to have note associated to user that submited it' do
+      expect(inquiry.notes.last.writer).to eq writer
     end
   end
 
