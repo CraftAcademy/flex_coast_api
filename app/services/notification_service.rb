b75d28a15af50875a2c0d2645a6dc62575f24cdd
Thomas <thomas@craftacademy.se>
2021-06-13 23:52:29 +0200

    FIX for non-configured email delivery method

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -3,7 +3,7 @@ require 'slack-notify'
 module NotificationService
   def self.new_inquiry(inquiry)
     # TODO: Configure mailer on production server
-    # InquiryMailer.created_email(inquiry).deliver
+    InquiryMailer.created_email(inquiry).deliver if Rails.env.test?
     client = SlackNotify::Client.new(
       webhook_url: Rails.application.credentials.dig(:slack, :webhook_url)
     )
