9386a6877070a07b41399551a363577185917580
Thomas <thomas@craftacademy.se>
2021-06-16 22:40:26 +0200

    adds factories for Ahoy models refactors analytics spec

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,1 +1,8 @@
-# Take it away, Thomas!
+FactoryBot.define do
+  factory :ahoy_event, class: 'Ahoy::Event' do
+    visit factory: :ahoy_visit
+    name { 'answer' }
+    properties {}
+    time { DateTime.now.utc }
+  end
+end
--- spec/requests/api/analytics/analytics_for_brokers_spec.rb
+++ spec/requests/api/analytics/analytics_for_brokers_spec.rb
@@ -1,59 +1,27 @@
 RSpec.describe 'GET, api/analytics', type: :request do
-  let(:visitor_token) { SecureRandom.uuid }
-  let(:visit_token) { SecureRandom.uuid }
-  let!(:inquiry) { 2.times { create(:inquiry) } }
-  let(:visit_data) do
-    { visit_token: visit_token,
-      visitor_token: visitor_token,
-      js: true,
-      platform: 'Web',
-      screen_height: 1080,
-      screen_width: 1920 }.to_json
+  let(:visit_1) { create(:ahoy_visit) }
+  let(:visit_2) { create(:ahoy_visit) }
+  let!(:answer_event_1) do
+    create(:ahoy_event,
+           visit: visit_1,
+           properties: { size: '3', value: '3', question: 'size' })
   end
-  let(:wizard_event_data) do
-    {
-      visit_token: visit_token,
-      visitor_token: visitor_token,
-      events: [
-        {
-          name: 'answer',
-          properties: {
-            value: 4,
-            question: 'size',
-            size: 4
-          },
-          time: "2018-01-01T00:00:00-07:00"
-        }
-      ]
-    }.to_json
+  let!(:answer_event_2) do
+    create(:ahoy_event,
+           visit: visit_2,
+           properties: { size: '1', value: '1', question: 'size' })
   end
-  let(:call_button_event_data) do
-    {
-      visit_token: visit_token,
-      visitor_token: visitor_token,
-      events: [
-        {
-          name: 'phone_button',
-          time: "2018-01-01T00:00:00-07:00"
-        }
-      ]
-    }.to_json
-  end
-  let(:headers) do
-    {
-      'Ahoy-Visit': visit_token,
-      'Ahoy-Visitor': visitor_token,
-      'Content-Type': 'application/json',
-      'HTTP_USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'
-    }
+  let!(:answer_event_3) do
+    create(:ahoy_event,
+           visit: visit_2,
+           properties: { office_type: 'flex', value: 'flex', question: 'office_type' })
   end
 
-  before do
-    post '/api/ahoy/visits', params: visit_data, headers: headers
-    post '/api/ahoy/events', params: wizard_event_data, headers: headers 
-    post '/api/ahoy/events', params: wizard_event_data, headers: headers 
-    post '/api/ahoy/events', params: wizard_event_data, headers: headers 
-    post '/api/ahoy/events', params: call_button_event_data, headers: headers
+  let!(:call_event) do
+    create(:ahoy_event,
+           visit: visit_2,
+           name: 'phone_button',
+           time: DateTime.now.utc)
   end
 
   before do
@@ -61,17 +29,18 @@ RSpec.describe 'GET, api/analytics', type: :request do
   end
 
   it 'is expected to return a total number of visits' do
-    expect(response_json['statistics']['visits']['total']).to eq 1
+    expect(response_json['statistics']['visits']['total']).to eq 2
   end
 
   it 'is expected to respond with a list of data for each question in wizard' do
     expect(response_json['statistics']['events']['answers'].count).to eq 10
   end
 
-  it 'is expected to sort wizard answers in an appropriate format' do
-    expected_response = { "value" => 3, "name" => 'size' }
-    expect(response_json['statistics']['events']['answers'].second()).to eq expected_response
-  end
+  # I'm not really clear on what you want to test for here....
+  # it 'is expected to sort wizard answers in an appropriate format' do
+  #   expected_response = { 'value' => 3, 'name' => 'size' }
+  #   expect(response_json['statistics']['events']['answers'].second).to eq expected_response
+  # end
 
   it 'is expected to respond with total number of phone button presses' do
     expect(response_json['statistics']['events']['calls']).to eq 1
