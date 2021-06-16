13cf5a9c5b9e2cf6d40bef2d04fa83a75af62202
lucas <laksteelhouse@gmail.com>
2021-06-16 18:13:47 +0200

    Removes typo

--- spec/requests/api/analytics/analytics_providers_spec.rb
+++ spec/requests/api/analytics/analytics_providers_spec.rb
@@ -10,14 +10,6 @@ RSpec.describe 'GET, api/analytics', type: :request do
       screen_height: 1080,
       screen_width: 1920 }.to_json
   end
-  let(:visit_headers) do
-    {
-      'Ahoy-Visit': visit_token,
-      'Ahoy-Visitor': visitor_token,
-      'Content-Type': 'application/json',
-      'HTTP_USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'
-    }
-  end
   let(:wizard_event_data) do
     {
       visit_token: visit_token,
@@ -35,7 +27,6 @@ RSpec.describe 'GET, api/analytics', type: :request do
       ]
     }.to_json
   end
-
   let(:call_button_event_data) do
     {
       visit_token: visit_token,
@@ -48,13 +39,21 @@ RSpec.describe 'GET, api/analytics', type: :request do
       ]
     }.to_json
   end
+  let(:headers) do
+    {
+      'Ahoy-Visit': visit_token,
+      'Ahoy-Visitor': visitor_token,
+      'Content-Type': 'application/json',
+      'HTTP_USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'
+    }
+  end
 
   before do
-    post '/api/ahoy/visits', params: visit_data, headers: visit_headers
-    post '/api/ahoy/events', params: wizard_event_data, headers: visit_headers
-    post '/api/ahoy/events', params: wizard_event_data, headers: visit_headers
-    post '/api/ahoy/events', params: wizard_event_data, headers: visit_headers
-    post '/api/ahoy/events', params: call_button_event_data, headers: visit_headers
+    post '/api/ahoy/visits', params: visit_data, headers: headers
+    post '/api/ahoy/events', params: wizard_event_data, headers: headers 
+    post '/api/ahoy/events', params: wizard_event_data, headers: headers 
+    post '/api/ahoy/events', params: wizard_event_data, headers: headers 
+    post '/api/ahoy/events', params: call_button_event_data, headers: headers
   end
 
   before do
