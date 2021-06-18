757e31253209070839485871da3bb3fbf6195f28
oliverochman <oliver.ochman@hotmail.com>
2021-06-18 11:14:18 +0200

    add started_email_sent column to inquiry and unit tests for it

--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -1,64 +1,7 @@
 RSpec.describe Inquiry, type: :model do
   let!(:mail_delivery) { ActionMailer::Base.deliveries }
 
-  describe 'Inquiry status events' do
-    let(:pending_inquiry) { create(:inquiry, inquiry_status: 'pending') }
-    let(:started_inquiry) { create(:inquiry, inquiry_status: 'started') }
-    let(:done_inquiry) { create(:inquiry, inquiry_status: 'done') }
-
-    describe 'Pending inquiry' do
-      it 'is expected to be able to ":start' do
-        expect{
-          pending_inquiry.start
-        }
-        .not_to raise_error
-      end
-
-      it 'is expected to not be able to ":finish"' do
-        expect{
-          pending_inquiry.finish
-        }
-        .to raise_error(StandardError)
-        .with_message("You can't perform this on an inquiry that is 'pending'")
-      end
-    end
-
-    describe 'Started inquiry' do
-      it 'is expected to be able to ":set_to_pending"' do
-        expect{
-          started_inquiry.set_to_pending
-        }
-        .not_to raise_error
-      end
-
-      it 'is expected to be able to "finish"' do
-        expect{
-          started_inquiry.finish
-        }
-        .not_to raise_error
-      end
-    end
-
-    describe 'Done inquiry' do
-      it 'is expected to not be able to ":set_to_pending"' do
-        expect{
-          done_inquiry.set_to_pending
-        }
-        .to raise_error(StandardError)
-        .with_message("You can't perform this on an inquiry that is 'done'")
-      end
-
-      it 'is expected to not be able to ":start"' do
-        expect{
-          done_inquiry.start
-        }
-        .to raise_error(StandardError)
-        .with_message("You can't perform this on an inquiry that is 'done'")
-      end
-    end
-  end
-
-  describe 'db table' do
+  describe 'Db table' do
     it {
       is_expected.to have_db_column(:size)
         .of_type(:integer)
@@ -70,6 +13,10 @@ RSpec.describe Inquiry, type: :model do
     it {
       is_expected.to have_db_column(:flexible)
         .of_type(:integer)
+    }
+    it {
+      is_expected.to have_db_column(:started_email_sent)
+        .of_type(:boolean)
     }
     it {
       is_expected.to have_db_column(:peers)
@@ -137,6 +84,14 @@ RSpec.describe Inquiry, type: :model do
       expect { create(:inquiry) }
         .to change { mail_delivery.count }.from(0).to(2)
     end
+
+    it 'is expected to only send notification email of inquiry started once' do
+      inquiry = create(:inquiry)
+      inquiry.start
+      inquiry.set_to_pending
+      inquiry.start
+      binding.pry
+    end
   end
 
   describe 'Factory' do
@@ -144,4 +99,62 @@ RSpec.describe Inquiry, type: :model do
       expect(create(:inquiry)).to be_valid
     end
   end
+
+  describe 'Inquiry status events' do
+    let(:pending_inquiry) { create(:inquiry, inquiry_status: 'pending') }
+    let(:started_inquiry) { create(:inquiry, inquiry_status: 'started') }
+    let(:done_inquiry) { create(:inquiry, inquiry_status: 'done') }
+
+    describe 'Pending inquiry' do
+      it 'is expected to be able to ":start' do
+        expect{
+          pending_inquiry.start
+        }
+        .not_to raise_error
+      end
+
+      it 'is expected to not be able to ":finish"' do
+        expect{
+          pending_inquiry.finish
+        }
+        .to raise_error(StandardError)
+        .with_message("You can't perform this on an inquiry that is 'pending'")
+      end
+    end
+
+    describe 'Started inquiry' do
+      it 'is expected to be able to ":set_to_pending"' do
+        expect{
+          started_inquiry.set_to_pending
+        }
+        .not_to raise_error
+      end
+
+      it 'is expected to be able to "finish"' do
+        expect{
+          started_inquiry.finish
+        }
+        .not_to raise_error
+      end
+    end
+
+    describe 'Done inquiry' do
+      it 'is expected to not be able to ":set_to_pending"' do
+        expect{
+          done_inquiry.set_to_pending
+        }
+        .to raise_error(StandardError)
+        .with_message("You can't perform this on an inquiry that is 'done'")
+      end
+
+      it 'is expected to not be able to ":start"' do
+        expect{
+          done_inquiry.start
+        }
+        .to raise_error(StandardError)
+        .with_message("You can't perform this on an inquiry that is 'done'")
+      end
+    end
+  end
+
 end
